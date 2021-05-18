60af3d13866203f3dcab588dbf74d0ecbf187593
Ashkan Ashtiani <ashcan@3dln.com>
2021-05-18 15:48:17 +0430

    current user

--- src/routes/current-user.ts
+++ src/routes/current-user.ts
@@ -1,9 +1,22 @@
 import express from 'express';
+import jwt from 'jsonwebtoken';
 
 const router = express.Router()
 
 router.get('/api/users/currentuser', (req, res) => {
-    res.send("hello from current user route")
+
+    // First check if jwt is presented in the session object
+    if (!req.session?.jwt) {
+        return res.send({ currentUser: null })
+    }
+
+    // verify the jwt
+    try {
+        const payload = jwt.verify(req.session.jwt, process.env.JWT_KEY!);
+        res.send({ currentUser: payload })
+    } catch (error) {
+        res.send({ currentUser: null })
+    }
 })
 
 export { router as currentUserRouter }
